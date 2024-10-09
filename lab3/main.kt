0b22b860b99095345d3a2a921b4898a2bff51301
PortikSzabolcs <99720038+PortikSzabolcs@users.noreply.github.com>
2024-10-09 18:03:42 +0300

    Delete lab3 directory

--- lab3/main.kt
+++
@@ -1,8 +0,0 @@
-package lab3
-
-data class Item(val question: String, val answers: List<String>, val correct: Int)
-
-fun main() {
-    val quizController = ItemController(ItemService(ItemRepository))
-    quizController.quiz(5)
-}
