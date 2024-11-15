45ab9027fee8c60ceadc30d02f2e284b7dd6b4a7
PortikSzabolcs <portik.szabolcs@student.ms.sapientia.ro>
2024-11-15 20:10:40 +0200

    Lab7+: New logo + colors

--- RecipeHub/app/src/main/java/com/example/recipehub/activity/SplashActivity.kt
+++ RecipeHub/app/src/main/java/com/example/recipehub/activity/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recipehub.R
 
@@ -12,12 +13,13 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 1500)
     }
 
     override fun onStart() {
